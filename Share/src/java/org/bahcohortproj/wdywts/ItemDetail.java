91e6385f9a60dc51034eba55aa871dbf27932e8f
jay <jay@jay-Studio-1737>
2012-11-17 23:37:32 -0500

    Added class ItemDetail.java based on DB Schema notes.

---
+++ Share/src/java/org/bahcohortproj/wdywts/ItemDetail.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bahcohortproj.wdywts;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jay
+ */
+@Entity
+public class ItemDetail implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long itemId;
+    private String itemName;
+    private String itemDescription;
+    private int categoryId;
+    
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @return the itemDescription
+     */
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    /**
+     * @param itemDescription the itemDescription to set
+     */
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    /**
+     * @return the categoryId
+     */
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    /**
+     * @param categoryId the categoryId to set
+     */
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+    
+}
